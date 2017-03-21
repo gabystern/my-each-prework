975887ab203b03b712460e2b487af68077e23f38
Gaby Stern <stern.gabrielle@gmail.com>
2017-03-21 14:45:49 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,19 @@
-def my_each # put argument(s) here
-  # code here
+def my_each (array)
+  i = 0
+  new_array = []
+  if array.length == 0
+    "This block should not run!"
+  else while i < array.length
+    yield array[i]
+    new_array.push(array[i])
+    i = i+1
+    end
+  end
+  new_array
+end
+
+collection = ["hi", "hello", "eek"]
+
+my_each(collection) do |item|
+  item
 end
